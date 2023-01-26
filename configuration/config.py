9d243fcfe3b0f6b09667decb8fd6b834c57cb27e
Gilad Carmel <giladcarmel22@gmail.com>
2023-01-26 21:23:12 +0200

    added defaults to config

--- configuration/config.py
+++ configuration/config.py
@@ -1,15 +1,13 @@
 from pydantic import BaseSettings
-
-#SQLALCHEMY_DB_URL = f''
-
+import configuration.defaults as defaults
 
 class Settings(BaseSettings):
-    POSTGRES_HOST: str = "localhost"
-    POSTGRES_PORT: int = 5432
-    POSTGRES_USER: str = "root"
-    POSTGRES_PASSWORD: str = "root"
-    POSTGRES_DB: str = "fastapi"
-    SQLALCHEMY_DB_URL: str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    POSTGRES_HOST: str = defaults.POSTGRES_HOST
+    POSTGRES_PORT: int = defaults.POSTGRES_PORT
+    POSTGRES_USER: str = defaults.POSTGRES_USER
+    POSTGRES_PASSWORD: str = defaults.POSTGRES_PASSWORD
+    POSTGRES_DB: str = defaults.POSTGRES_DB
+    SQLALCHEMY_DB_URL: str = defaults.SQLALCHEMY_DB_URL
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
