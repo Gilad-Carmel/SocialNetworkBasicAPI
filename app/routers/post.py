739ce76a6f0108fc7e5678ff075bca84d6029e83
Gilad Carmel <giladcarmel22@gmail.com>
2023-01-26 20:34:54 +0200

    added vote-post response to single post

--- app/routers/post.py
+++ app/routers/post.py
@@ -32,12 +32,14 @@ def get_posts(db: Session = Depends(get_db),
     return posts
 
 
-@router.get("/{id}")
+@router.get("/{id}", response_model=schemas.PostVotes)
 def get_post(id: int,
              db: Session = Depends(get_db),
              current_user: int = Depends(oauth2.get_current_user)):
 
-    post = db.query(models.Post).filter(models.Post.id == id).first()
+    post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(
+            models.Post.id).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} was not found")
